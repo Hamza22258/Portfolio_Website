5b4e09be87f183ebc5122771cd2e9991060efeec
Hamza Malik <hamzamalik22258@gmail.com>
2023-06-28 07:10:16 -0700

    Added style for bullets

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -26,29 +26,29 @@ export default function Experience() {
             <h2>Responsibilities</h2>
             <ul>
               <li>
-                <span>✤ </span> Worked on wide variety of technologies and
+                <span>⚡ </span> Worked on wide variety of technologies and
                 practices such CI/CD pipelines, containerization and cloud
                 computing.
               </li>
               <li>
-                <span>✤ </span> Successfully delivered complex software projects
-                within designated timeframes, demonstrating strong project
-                management and technical skills.
+                <span>⚡ </span> Successfully delivered complex software
+                projects within designated timeframes, demonstrating strong
+                project management and technical skills.
               </li>
 
               <li>
-                <span>✤ </span> Provided my skills in Full Stack development
+                <span>⚡ </span> Provided my skills in Full Stack development
                 with recent focus on performance and SEO.
               </li>
 
               <li>
-                <span>✤ </span> Implemented innovative solutions that improved
+                <span>⚡ </span> Implemented innovative solutions that improved
                 efficiency, scalability, or user experience, resulting in
                 positive feedback from clients or end-users.
               </li>
 
               <li>
-                <span>✤ </span> Led a cross-functional team in the development
+                <span>⚡ </span> Led a cross-functional team in the development
                 of a high- profile software product, showcasing leadership
                 abilities and effective collaboration with diverse stakeholders.
               </li>
@@ -75,17 +75,17 @@ export default function Experience() {
             <h2>Responsibilities</h2>
             <ul>
               <li>
-                <span>✤ </span> Provided multi-language support to the clients
+                <span>⚡ </span> Provided multi-language support to the clients
               </li>
               <li>
-                <span>✤ </span> Provided my programming skills to solve complex
+                <span>⚡ </span> Provided my programming skills to solve complex
                 problem
               </li>
               <li>
-                <span>✤ </span> Provided my skills in web development
+                <span>⚡ </span> Provided my skills in web development
               </li>
               <li>
-                <span>✤ </span> Advised and managed major stack changes
+                <span>⚡ </span> Advised and managed major stack changes
               </li>
             </ul>
           </div>
