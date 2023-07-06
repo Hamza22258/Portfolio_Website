ea70f33029a69c0c3666b61e479ad0af37809ac0
Hamza Malik <hamzamalik22258@gmail.com>
2023-07-06 13:10:44 -0700

    Added email functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,45 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./contact.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import emailjs from "emailjs-com";
 export default function Contact() {
-  const [message, setMessage] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+  const [message, setMessage] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage((previousState) => !previousState);
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  // Set up your EmailJS service credentials
+  const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const publicID = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+  const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+
+  // Create the template parameters with the form data
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(serviceID, templateID, form.current, publicID).then(
+      (result) => {
+        setEmailSent(true);
+        setTimeout(() => setEmailSent(false), 3000);
+        setEmail("");
+        setMessage("");
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -16,11 +47,22 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="email"
+            value={email}
+            name="email"
+            onChange={handleEmailChange}
+            required
+          />
+          <textarea
+            value={message}
+            name="message"
+            onChange={handleMessageChange}
+            required
+          ></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
+          {emailSent && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
       </div>
       <a href="#intro" className="intro-arrow">
